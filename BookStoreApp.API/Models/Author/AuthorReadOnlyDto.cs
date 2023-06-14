2a20e34d890bd126dea90b1a4e8527452a93cf57
tashabes <natasha_abel@hotmail.co.uk>
2023-06-14 12:23:11 +0100

    Completed Author API setup

---
+++ BookStoreApp.API/Models/Author/AuthorReadOnlyDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.API.Models.Author
+{
+    public class AuthorReadOnlyDto : BaseDto
+    {
+
+        public string FirstName { get; set; }
+     
+        public string LastName { get; set; }
+
+      
+        public string Bio { get; set; }
+    }
+}
---
+++ BookStoreApp.API/Static/Messages.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Identity.Client;
+
+namespace BookStoreApp.API.Static
+{
+    public static class Messages
+    {
+        public const string Error500Message = "There was an error completing your request.  Please try again later.";
+    }
+}
